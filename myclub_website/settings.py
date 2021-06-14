d6adf12bd9ac6ebd1919d32eb8c1df7045af9eb9
Liang Wei <weiliangtuina@gmail.com>
2021-06-14 18:35:53 +0800

    psql_myclub

--- myclub_website/settings.py
+++ myclub_website/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'myclub_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myclub',
+        'USER': 'weiliang',
+        'PASSWORD': 'fendouba12',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
